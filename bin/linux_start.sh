5ce282fb29b16d0b449f29d36e6335b2fa742667
baogejin <cugbbaoge@163.com>
2022-12-15 17:32:53 +0800

    linux启动时删除旧的log

--- bin/linux_start.sh
+++ bin/linux_start.sh
@@ -1,7 +1,7 @@
 ulimit -c unlimited
 
 dir=$gs/bin
-
+rm -rf $gs/log/*
 GOTRACEBACK=crash $dir/server -node=gateway 1>/dev/null 2>&1 &
 GOTRACEBACK=crash $dir/server -node=logic 1>/dev/null 2>&1 &
 
